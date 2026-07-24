36681c369d3c2ba028e6de0fa0b078bb1777980d
teymuruae <baratov.teymur@yandex.ru>
2026-07-24 20:43:32 +0300

    9.1 Введение в grpcio. Практика работы с grpcio

---
+++ grpcio_open_debit_card_account.py
@@ -0,0 +1,31 @@
+import grpc
+
+from contracts.services.gateway.users.users_gateway_service_pb2_grpc import UsersGatewayServiceStub
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserRequest, CreateUserResponse
+from contracts.services.gateway.accounts.accounts_gateway_service_pb2_grpc import AccountsGatewayServiceStub
+from contracts.services.gateway.accounts.rpc_open_debit_card_account_pb2 import OpenDebitCardAccountRequest, \
+    OpenDebitCardAccountResponse
+from tools.fakers import fake
+
+channel = grpc.insecure_channel("localhost:9003")
+users_stub = UsersGatewayServiceStub(channel)
+account_stub = AccountsGatewayServiceStub(channel)
+
+create_user_request = CreateUserRequest(
+    email=fake.email(),
+    last_name=fake.last_name(),
+    first_name=fake.first_name(),
+    middle_name=fake.middle_name(),
+    phone_number=fake.phone_number()
+)
+
+create_user_response: CreateUserResponse = users_stub.CreateUser(create_user_request)
+print("Create user response: ", create_user_response)
+
+open_debit_card_account_request = OpenDebitCardAccountRequest(
+    user_id=create_user_response.user.id
+)
+
+open_debit_card_account_response: OpenDebitCardAccountResponse = account_stub.OpenDebitCardAccount(
+    open_debit_card_account_request)
+print("Open debit card account response: ", open_debit_card_account_response)
